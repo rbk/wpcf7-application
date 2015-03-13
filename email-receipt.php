982097dcd08f006c10390b1c7dfea4d23e63e197
richard <richard@gurustu.co>
2015-03-13 15:42:37 -0600

    added stronger tags

--- email-receipt.php
+++ email-receipt.php
@@ -1,35 +1,41 @@
-<h1>Employment Application<h1>
-Applicant Name: [fname] [mname] [lname]
-Desired Position: [position]
-Contact Phone: [phone]
-Birthday: [birthday]
-Date Available: [start]
-Address 1: [address1]
-Address 2 [address2]
-City: [city]
-State: [state]
-Zip code: [zipcode]
+<h2>Employment Application</h2>
+<table>
+	<tr>
+		<td><strong>Applicant Name: </strong></td>
+		<td>[fname] [mname] [lname]</td>
+	</tr>
+</table>
+
+<strong>Desired Position: </strong>[position]
+<strong>Contact Phone: </strong>[phone]
+<strong>Birthday: </strong>[birthday]
+<strong>Date Available: </strong>[start]
+<strong>Address 1: </strong>[address1]
+<strong>Address 2 </strong>[address2]
+<strong>City: </strong>[city]
+<strong>State: </strong>[state]
+<strong>Zip code: </strong>[zipcode]
 
 <h2>Work History</h2>
 
 <h3>Job 1:</h3>
 
-Company: [company1]
-Company Phone: [company_phone1]
-Position held: [position1]
-Job Description: [description1]
-Dates Employed: [start1] to [end1]
-Reason For Leaving:
+<strong>Company: </strong>[company1]
+<strong>Company Phone: </strong>[company_phone1]
+<strong>Position held: </strong>[position1]
+<strong>Job Description: </strong>[description1]
+<strong>Dates Employed: </strong>[start1] to [end1]
+<strong>Reason For Leaving:</strong>
 [reason1]
 
 <h3>Job 2:</h3>
 
-Company: [company2]
-Company Phone: [company_phone2]
-Position held: [position2]
-Job Description: [description2]
-Dates Employed: [start2] to [end2]
-Reason For Leaving:
+<strong>Company: </strong>[company2]
+<strong>Company Phone: </strong>[company_phone2]
+<strong>Position held: </strong>[position2]
+<strong>Job Description: </strong>[description2]
+<strong>Dates Employed: </strong>[start2] to [end2]
+<strong>Reason For Leaving:</strong>
 [reason2]
 
 <h2>Addition Information</h2>
