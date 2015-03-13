118b207e10fdd69a69be50af09d63bb8d6415805
richard <richard@gurustu.co>
2015-03-13 15:34:50 -0600

    added receipt

---
+++ email-receipt.php
@@ -0,0 +1,36 @@
+<h1>Employment Application<h1>
+Applicant Name: [fname] [mname] [lname]
+Desired Position: [position]
+Contact Phone: [phone]
+Birthday: [birthday]
+Date Available: [start]
+Address 1: [address1]
+Address 2 [address2]
+City: [city]
+State: [state]
+Zip code: [zipcode]
+
+<h2>Work History</h2>
+
+<h3>Job 1:</h3>
+
+Company: [company1]
+Company Phone: [company_phone1]
+Position held: [position1]
+Job Description: [description1]
+Dates Employed: [start1] to [end1]
+Reason For Leaving:
+[reason1]
+
+<h3>Job 2:</h3>
+
+Company: [company2]
+Company Phone: [company_phone2]
+Position held: [position2]
+Job Description: [description2]
+Dates Employed: [start2] to [end2]
+Reason For Leaving:
+[reason2]
+
+<h2>Addition Information</h2>
+[summary]
