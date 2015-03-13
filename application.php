82a42361f35e8ebe2b73f87755f0d1ed30608ee3
richard <richard@gurustu.co>
2015-03-13 15:27:11 -0600

    init

---
+++ application.php
@@ -0,0 +1,144 @@
+
+
+<fieldset>
+	<legend>Basic</legend>	
+	<div class="row">
+		<div class="re re6">
+			<label for="">Position Applying For</label>
+			[text position]
+		</div>
+		<div class="re re6">
+			<label for="">Phone Number</label>
+			[text* phone]
+		</div>
+	</div>
+	<div class="row">	
+		<div class="re re4">
+			<label>First Name</label>
+			[text* fname]
+		</div>
+		<div class="re re4">
+			<label>Middle Initial</label>
+			[text mname]
+		</div>
+		<div class="re re4">
+			<label>Last Name</label>
+			[text* lname]
+		</div>
+	</div>
+	<div class="row">
+		<div class="re re6">
+			<label>Birthday</label>
+			[text birthday]
+		</div>
+		<div class="re re6">
+			<label>Available Start Date</label>
+			[text start]
+		</div>
+	</div>
+	<div class="row">
+		<div class="re re12">
+			<label>Address 1</label>
+			[text address1]
+		</div>
+		<div class="re re12">
+			<label>Address 2</label>
+			[text address2]
+		</div>
+	</div>
+	<div class="row">
+		<div class="re re4">
+			<label>City</label>
+			[text city]
+		</div>
+		<div class="re re4">
+			<label>State</label>
+			[text state]
+		</div>
+		<div class="re re4">
+			<label for="">Zip Code</label>
+			[text zipcode]
+		</div>
+	</div>
+</fieldset>
+<fieldset>
+	<legend>Work History</legend>
+	<div class="row">
+		<div class="re re6">
+			<label for="">Company</label>
+			[text company1]
+		</div>
+		<div class="re re6">
+			<label for="">Company Phone</label>
+			[text company_phone1]
+		</div>
+	</div>
+	<div class="row">
+		<div class="re re12">
+			<label for="">Position</label>
+			[text position1]
+		</div>
+		<div class="re re12">	
+			<label for="">Job Description</label>
+			[textarea description1]
+		</div>
+	</div>
+	<div class="row">
+		<div class="re re6">
+			<label for="">Start Date</label>
+			[text start1]
+		</div>
+		<div class="re re6">
+			<label for="">End Date</label>
+			[text end1]
+		</div>
+		<re class="re12">
+			<label for="">Reason for leaving</label>
+			[textarea reason1]
+		</re>
+	</div>
+	<hr>
+		<div class="row">
+		<div class="re re6">
+			<label for="">Company</label>
+			[text company2]
+		</div>
+		<div class="re re6">
+			<label for="">Company Phone</label>
+			[text company_phone2]
+		</div>
+	</div>
+	<div class="row">
+		<div class="re re12">
+			<label for="">Position</label>
+			[text position2]
+		</div>
+		<div class="re re12">	
+			<label for="">Job Description</label>
+			[textarea description2]
+		</div>
+	</div>
+	<div class="row">
+		<div class="re re6">
+			<label for="">Start Date</label>
+			[text start2]
+		</div>
+		<div class="re re6">
+			<label for="">End Date</label>
+			[text end2]
+		</div>
+		<re class="re12">
+			<label for="">Reason for leaving</label>
+			[textarea reason2]
+		</re>
+	</div>
+	<div class="row">
+		<label for="">Summarize any additional information necessary to describe your full qualifications for the position for which you are applying.</label>
+		[textarea summary]
+	</div>
+	<div class="row">
+		<div class="re re12">
+			[submit "Submit Application"]
+		</div>
+	</div>
+</fieldset>
